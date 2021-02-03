32edd4ac4d7f4a5817814b98ac0a9fe836156aad
Priya <priyaaditya2207@gmail.com>
2021-02-03 22:27:22 +0530

    something something

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MyApp from './Components/MyApp';
-import './CSS/style.css';
-
-
-ReactDOM.render(<MyApp/>,document.getElementById('root'));
